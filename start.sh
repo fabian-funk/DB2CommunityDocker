04c10adbbc1d44fe75ecc037cae57c1678dac26f
Fabian Funk <fabianfunk@Fabians-MacBook-Air.local>
2022-07-13 13:32:00 +0200

    added start

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run -d -h db2server --name db2server --network db2net --rm --privileged=true -p 50000:50000 --env-file .env_list ibmcom/db2
+docker run -d -h db2server --name db2server --rm --privileged=true -p 50000:50000 --env-file .env_list ibmcom/db2
